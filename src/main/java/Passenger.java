67ca6c46ec8c5023293c7a1dbb6b58ccb66a39cb
ghr85 <ghrenfrew@gmail.com>
2019-04-01 18:24:11 +0100

    added countLuggage to passenger class

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,2 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Passenger {
+    private String name;
+    private ArrayList<Luggage> luggage;
+
+    public Passenger(String name){
+        this.name = name;
+        this.luggage = new ArrayList<>();
+    }
 }
--- src/test/java/TestPassenger.java
+++ src/test/java/TestPassenger.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestPassenger {
+    public Passenger passenger ;
+    public Luggage luggage;
+
+    @Before
+
+    public void  before(){
+        luggage = new Luggage(23.0, LuggageType.CARGO);
+        passenger = new Passenger("Davey");
+    }
+
+    @Test
+
+    public void startsWithNoLuggage(){
+        assertEquals(0,passenger.luggageCount());
+    }
 }
