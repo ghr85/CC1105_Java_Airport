28fbe3783126899b8b53d5ad39e6eb1bfa1cbb63
ghr85 <ghrenfrew@gmail.com>
2019-04-02 06:57:31 +0100

    added get destination functionality to Flight model class

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -5,6 +5,27 @@
 //        * destination
 
 public class Flight {
+    private Plane plane;
+    private String flightNumber;
+    private AirportCode destination;
+
+    public Flight(Plane plane, String flightNumber, AirportCode destination){
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+    }
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination.getDestination();
+    }
+
 
 
 }
--- src/test/java/TestFlight.java
+++ src/test/java/TestFlight.java
@@ -1,2 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class TestFlight {
+
+    private Plane plane;
+    private Flight flight;
+
+
+    @Before
+    public void before(){
+       plane = new Plane(PlaneType.BIPLANE,Airline.RYANAIR) ;
+       flight = new Flight(plane,"ZX213",AirportCode.GLA);
+    }
+
+    @Test
+    public void hasPlane(){
+        assertEquals(plane,flight.getPlane());
+    }
+
+    @Test
+    public void hasFlightNumber(){
+        assertEquals("ZX213", flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestination(){
+        assertEquals("Glasgow", flight.getDestination());
+    }
+
+
 }
